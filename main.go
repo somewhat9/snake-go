80e51d9f0b0d5a0f27a952005526b08111b3512a
somewhat9 <179911515+somewhat9@users.noreply.github.com>
2025-07-20 02:42:26 -0400

    Implemented Movement System
    Implemented a system for moving the snake and updating the board.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/vector"
@@ -25,8 +26,6 @@ const (
 	ScreenHeight = GridHeight + GridHeightOffset
 )
 
-var Board [GridSquaresHeight][GridSquaresWidth]uint8
-
 var Colors = map[uint8]color.Color{
 	0: color.Black, 
 	1: color.RGBA{0, 255, 0, 255}, 
@@ -35,10 +34,40 @@ var Colors = map[uint8]color.Color{
 
 type Game struct{
 	Snake
+	Board [GridSquaresHeight][GridSquaresWidth]uint8
+	LastTick time.Time
 }
 
 type Snake struct{
-	Position
+	Body []Position
+	Dir Position
+}
+
+func (g *Game) Tick() {
+	head := g.Body[0]
+	newHead := Position{X: head.X + g.Dir.X, Y: head.Y + g.Dir.Y}
+
+	/* Debug Prints
+	fmt.Print(newHead)
+	fmt.Print(" ")
+	fmt.Print(head)
+	fmt.Print(" ")
+	fmt.Println(g.Board[int(newHead.Y)][int(newHead.X)])
+	*/
+
+	switch g.Board[int(newHead.Y)][int(newHead.X)] {
+	case 0:
+		g.Body = append([]Position{newHead}, g.Body...)
+		tail := g.Body[len(g.Body)-1]
+		g.Body = g.Body[:len(g.Body)-1]
+		g.Board[int(newHead.Y)][int(newHead.X)] = 1
+		g.Board[int(tail.Y)][int(tail.X)] = 0
+	case 1:
+		// end game
+	case 2:
+		g.Body = append([]Position{newHead}, g.Body...)
+		g.Board[int(newHead.Y)][int(newHead.X)] = 1
+	}
 }
 
 type Position struct {
@@ -46,12 +75,29 @@ type Position struct {
 }
 
 func (g *Game) Update() error {
+	switch {
+	case ebiten.IsKeyPressed(ebiten.KeyArrowUp):
+		g.Dir = Position{X: 0, Y: -1}
+	case ebiten.IsKeyPressed(ebiten.KeyArrowDown):
+		g.Dir = Position{X: 0, Y: 1}
+	case ebiten.IsKeyPressed(ebiten.KeyArrowRight):
+		g.Dir = Position{X: 1, Y: 0}
+	case ebiten.IsKeyPressed(ebiten.KeyArrowLeft):
+		g.Dir = Position{X: -1, Y: 0}
+	}
+
+	now := time.Now()
+	if now.Sub(g.LastTick) >= time.Second/4 {
+		g.LastTick = now
+		g.Tick()
+	}
+
 	return nil 
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.White)
-	for y, col := range Board {
+	for y, col := range g.Board {
 		for x, value := range col {
 			if ViewGrid {
 				vector.DrawFilledRect(screen, float32(x*SquareSize)+1+GridWidthOffset, float32(y*SquareSize)+1+GridHeightOffset, SquareSize-2, SquareSize-2, Colors[value], false)
@@ -70,7 +116,8 @@ func main() {
 	game := &Game{}
 	ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
 	ebiten.SetWindowTitle("Snake")
-	Board[ScreenHeight/SquareSize/2][ScreenWidth/SquareSize/2] = 1
+	game.Body = append(game.Body, Position{X: ScreenWidth/SquareSize/2, Y: ScreenHeight/SquareSize/2})
+	game.Board[ScreenHeight/SquareSize/2][ScreenWidth/SquareSize/2] = 1
 
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
